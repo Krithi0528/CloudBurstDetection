0c0446400b8f3ac580bcff1a9ab71e6d5682818e
Krithi0528 <120370422+Krithi0528@users.noreply.github.com>
2024-05-11 01:18:15 +0530

    cloud_burst_predictor.py

--- Backend/cloud_burst_predictor.py
+++ Backend/cloud_burst_predictor.py
@@ -1,5 +1,3 @@
-# cloud_burst_predictor.py
-
 import pandas as pd
 import requests
 import pickle
@@ -18,7 +16,6 @@ class CloudBurstPredictor(BaseEstimator):
         api_key = 'a6673e782011461b31f86f49a5d71adb'
         weather_data = self.fetch_weather_data(location, api_key)
         
-        # Process weather data to create features
         weather_features = {
             'Temperature': weather_data['main']['temp'],
             'Humidity': weather_data['main']['humidity'],
@@ -27,14 +24,10 @@ class CloudBurstPredictor(BaseEstimator):
         }
         weather_df = pd.DataFrame([weather_features])
         
-        # Load the trained model
         with open('random_forest_model.pkl', 'rb') as f:
             rf_model = pickle.load(f)
         
-        # Make predictions using the loaded model
         prediction = rf_model.predict(weather_df)
-        
-        # Return the prediction result
         if prediction[0] == 1:
             return "Cloud burst is predicted in the specified location."
         else:
